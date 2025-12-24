7a2c0c85ccfe2b704ba7671f816c1311185bb0f6
berktext99-eng <berktext99@gmail.com>
2025-12-24 21:56:49 +0300

    Update reactionRole.js

--- reactionRole.js
+++ reactionRole.js
@@ -1,31 +1,36 @@
 module.exports = (client) => {
 
-  // 🔧 AYARLAR
   const MESSAGE_ID = "1453358985569894400";
-  const EMOJI_ID = "1453458869899759677";
+  const EMOJI_ID = "1453458869899759677"; // <-- emoji ID burada
   const GIVE_ROLE_ID = "1452814296491884616";
   const REMOVE_ROLE_ID = "1452816860029390969";
 
   client.on("messageReactionAdd", async (reaction, user) => {
     if (user.bot) return;
-
     if (reaction.partial) await reaction.fetch();
 
-    if (
-      reaction.message.id === MESSAGE_ID &&
-      reaction.emoji.name === EMOJI
-    ) {
-      const member = await reaction.message.guild.members.fetch(user.id);
+    // yanlış mesaja basıldıysa
+    if (reaction.message.id !== MESSAGE_ID) return;
 
-      if (member.roles.cache.has(REMOVE_ROLE_ID)) {
-        await member.roles.remove(REMOVE_ROLE_ID);
-      }
+    // yanlış emoji ise
+    if (reaction.emoji.id !== EMOJI_ID) return;
 
+    const member = await reaction.message.guild.members.fetch(user.id);
+
+    // eski rolü al
+    if (member.roles.cache.has(REMOVE_ROLE_ID)) {
+      await member.roles.remove(REMOVE_ROLE_ID);
+    }
+
+    // yeni rolü ver
+    if (!member.roles.cache.has(GIVE_ROLE_ID)) {
       await member.roles.add(GIVE_ROLE_ID);
     }
   });
 
 };
+
+
 
 
 
